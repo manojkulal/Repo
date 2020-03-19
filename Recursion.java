65c0ee6c2c80a75e5fb1648ca2145a59a08860f6
manojkulal <manojkulal1993@gmail.com>
2020-03-19 19:13:37 +0530

    adding recursion.java

---
+++ Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printStar(10);
+
+	}
+	
+	public static void printStar(int n){
+	    if(n > 0){
+	        System.out.println("MANOJ");
+	        printStar(n-1);
+	    }
+	}
+}
